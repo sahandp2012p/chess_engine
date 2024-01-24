af08b8f6cf14d9d4e037abbef88fa83ff8a65ea8
Sahand Pourjavad <spourjavad2012@gmail.com>
2024-01-24 16:27:39 +0330

    Created code the generate evaluations on board positions

--- generate_data.py
+++ generate_data.py
@@ -1,9 +1,20 @@
 from generate_board import generate
+import chess.engine
 
 boards = []
 
-for _ in range(1000):
+for _ in range(100):
     try:
         boards.append(generate())
     except:
         boards.append(generate()) # Because bugs out for certain position
+
+engine = chess.engine.SimpleEngine.popen_uci('./stockfish-ubuntu-x86-64-avx2')
+
+def analyse(board):
+    info = engine.analyse(board, limit=chess.engine.Limit(depth=20))
+
+    return info['score'].relative.score(mate_score=100000)
+    
+evals = [analyse(board) for board in boards]
+engine.quit()
