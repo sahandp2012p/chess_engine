7af5c6981378170b342929a1a1f86bf7f3a80184
Sahand Pourjavad <spourjavad2012@gmail.com>
2024-01-24 19:01:57 +0330

    Generated more test cases

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ import utils
 
 boards = []
 
-for _ in range(100):
+for _ in range(1000):
     try:
         boards.append(generate())
     except:
@@ -13,7 +13,7 @@ for _ in range(100):
 engine = chess.engine.SimpleEngine.popen_uci('./stockfish-ubuntu-x86-64-avx2')
 
 def analyse(board):
-    info = engine.analyse(board, limit=chess.engine.Limit(depth=20))
+    info = engine.analyse(board, limit=chess.engine.Limit(depth=16))
 
     return info['score'].relative.score(mate_score=100000)
     
